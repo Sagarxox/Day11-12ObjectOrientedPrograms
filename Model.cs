bfc6d15e55e9c5afb8b9afaf74605cf4d5faa2a8
sagar <skatahre@gmail.com>
2022-12-12 23:03:40 +0530

    inventory list modeling

---
+++ Model.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagment
+{
+    public class Model
+    {
+        public string Name { get ; set; }
+        public int weight { get ; set; }
+        public int price { get  ; set; }
+    }
+}
